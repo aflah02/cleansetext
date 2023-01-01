52e3e46d3d36b143507c8fa06d45da833a8688d8
Aflah <72096386+aflah02@users.noreply.github.com>
2023-01-01 12:05:37 +0530

    Version Upgrade

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="cleansetext",
-    version="0.0.7",
+    version="0.0.8",
     description="A Python library for cleaning text data",
     long_description=README,
     long_description_content_type="text/markdown",
